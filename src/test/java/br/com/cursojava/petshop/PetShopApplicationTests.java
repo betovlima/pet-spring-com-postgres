e4335a1332d9fa891527fb1fffc0d6966b0ce9df
betovlima <betovlima@gmail.com>
2022-08-12 18:56:31 -0300

    Ajustes

--- src/test/java/br/com/cursojava/petshop/PetShopApplicationTests.java
+++ src/test/java/br/com/cursojava/petshop/PetShopApplicationTests.java
@@ -1,13 +1,10 @@
 package br.com.cursojava.petshop;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+//@SpringBootTest
 class PetShopApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    //@Test
+    void contextLoads() {
+    }
 
 }
