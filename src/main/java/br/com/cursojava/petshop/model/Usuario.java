4dd327a495b21f7e237b28c44d839a5682d7a0e9
roberto_lima <roberto_lima@sicredi.com.br>
2022-11-12 06:32:38 -0300

    Limpeza de código

--- src/main/java/br/com/cursojava/petshop/model/Usuario.java
+++ src/main/java/br/com/cursojava/petshop/model/Usuario.java
@@ -1,9 +1,11 @@
 package br.com.cursojava.petshop.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
